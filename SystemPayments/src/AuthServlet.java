2fe7d1de855681940e6762a0f077e102452fd2e9
nazgul131 <nazgul.cut@gmail.com>
2016-04-24 15:44:41 +0300

    Addd files

--- SystemPayments/src/AuthServlet.java
+++ SystemPayments/src/AuthServlet.java
@@ -1,5 +1,21 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * Created by Admin on 24.04.2016.
  */
-public class AuthServlet {
+public class AuthServlet extends HttpServlet
+{
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        response.getWriter().println("HelloWorld!");
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+
+    }
 }
--- SystemPayments/src/Main.java
+++ SystemPayments/src/Main.java
@@ -1,5 +1,24 @@
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
 /**
  * Created by Admin on 24.04.2016.
  */
-public class Main {
+public class Main
+{
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8080);
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        server.setHandler(context);
+
+        context.addServlet(new ServletHolder(new AuthServlet()), "/auth");
+        context.addServlet(new ServletHolder(new AccountsServlet()), "/accounts");
+        context.addServlet(new ServletHolder(new LimitsServlet()), "/limits");
+        context.addServlet(new ServletHolder(new PaymentsServlet()), "/payments");
+
+        server.start();
+        server.join();
+    }
 }
